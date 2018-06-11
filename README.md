0770f2e3614a7691174bcbfbb09b7db88f5296a4
euvbonk <seiste@yahoo.de>
2018-06-11 16:29:19 +0200

    Provide better cli arguments parsing and enable html transforming.

--- README.md
+++ README.md
@@ -1,26 +1,42 @@
 ## XML - XSLT - FO - Renderer
 
-Programm zur Transformation eines XML Dokumentes mit einer XSL-FO Vor-  
-lage zu einem PDF-Dokument.
+Programm zur Umwandlung einer XML in eine PDF oder HTML Datei mittels  
+einer XSL-FO oder XSL Vorlagedatei.
 
 ## Benutzung
 
 In einem Terminal in den Ordner mit dem entpackten Archiv wechseln und  
 folgendes in die Kommandozeile eingeben:
 
-bash bin/XmlXslFoRenderer [source].xml [template].xsl
+    Programmaufruf: bin/XmlXslFoRenderer[.bat] [-c <fop-config>] [-h] 
+                [-html <Dateiname>] [-pdf <Dateiname>] -xml <Datei> -xsl <Datei>
 
-Es sollte eine Datei mit Namen '[source].pdf' entstehen.
+    Programm zur Umwandlung einer XML in eine PDF oder HTML Datei mittels einer
+    XSL-FO oder XSL Vorlagedatei.
 
-[source] und [template] sind jeweils durch die richtigen Dateinamen zu  
-ersetzen und sollten sich in diesem Verzeichnis befinden.
+     -c,--config <fop-config>   FOP Transformationskonfigurationsdatei. Vorgabe:
+                                "fop-config.xconf"
+     -h,--help                  Diese Nachricht.
+     -html <Dateiname>          Ausgabeformat HTML.
+     -pdf <Dateiname>           Ausgabeformat PDF.
+     -xml <Datei>               XML Eingabedatei.
+     -xsl <Datei>               XSL Vorlagedatei.
+
+    Ist kein Ausgabedateiname angegeben, wird als Basis der XML Dateiname verwendet.
+
+
+`<Datei>` ist jeweils durch den richtigen Dateinamen zu  
+ersetzen und sollte sich in diesem Verzeichnis befinden.
 
 ## FOP - Konfiguration
 
 Zusätzliche Anpassungen für den FOP Transformationsprozess lassen sich  
-über die Datei 'fop-config.xconf' vornehmen.  Zum Beispiel:  Einbinden  
+über die Datei `fop-config.xconf` vornehmen.  Zum Beispiel:  Einbinden  
 anderer Schriftarten.
 Siehe auch https://xmlgraphics.apache.org/fop/2.1/fonts.html
 
 Die Datei wird automatisch beim Programmaufruf eingebunden, sofern sie  
 sich in diesem Verzeichnis befindet!
+
+Alternativ lässt sich auch eine andere Datei über den entsprechenden  
+Parameter einbinden.
--- src/main/java/com/github/euvbonk/XmlXslFoRenderer.java
+++ src/main/java/com/github/euvbonk/XmlXslFoRenderer.java
@@ -1,10 +1,10 @@
 package com.github.euvbonk;
 
+import java.awt.Desktop;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -16,6 +16,13 @@ import javax.xml.transform.sax.SAXResult;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.fop.apps.FOUserAgent;
 import org.apache.fop.apps.Fop;
 import org.apache.fop.apps.FopFactory;
@@ -25,38 +32,99 @@ import org.xml.sax.SAXException;
 
 /**
  * @author euvbonk
- * @version 31.05.2018
+ * @version 11.06.2018
  */
 public class XmlXslFoRenderer
 {
-    private static final String USAGE = "" +
-            "Programm zur Umwandlung einer XML in eine PDF Datei mittels\n" +
-            "einer XSL-FO Vorlagedatei.\n\n" +
-            "Ist kein Ausgabe-Name angegeben, wird als Basis der XML Datei-\n" +
-            "name verwendet.\n\n" +
-            "Programmaufruf:\n" +
-            "\tXmlXslFoRenderer[.bat] xml-datei xsl-fo-datei [ausgabe-name]\n";
+    private static final String FOP_CONFIG_FILE_NAME = "fop-config.xconf";
 
-    public static void main(String[] args) throws IOException, SAXException, TransformerException
+    private static final String USAGE = XmlXslFoRenderer.class.getSimpleName();
+    private static final String HEADER = "\nProgramm zur Umwandlung einer XML in eine PDF oder HTML Datei mittels " +
+                "einer XSL-FO oder XSL Vorlagedatei.\n\n";
+    private static final String FOOTER = "\nIst kein Ausgabedateiname angegeben, wird als Basis der XML Datei" +
+            "name verwendet.";
+
+    private static final Options OPTIONS = new Options();
+    private static final CommandLineParser COMMAND_LINE_PARSER = new DefaultParser();
+    private static final HelpFormatter HELP_FORMATTER = new HelpFormatter();
+
+    static
     {
-        if (args.length < 2)
-        {
-            System.out.println(USAGE);
-            System.exit(1);
-        }
-        new XmlXslFoRenderer().convertToPDF(args[0], args[1], args.length > 2 ? args[2] : null);
+        OPTIONS.addOption(
+                Option.builder("c").longOpt("config").optionalArg(true).hasArg().argName("fop-config").type(File.class)
+                        .desc("FOP Transformationskonfigurationsdatei. Vorgabe: \"" + FOP_CONFIG_FILE_NAME + "\"").build());
+        OPTIONS.addOption(
+                Option.builder("xml").required().hasArg().argName("Datei").type(File.class).desc("XML Eingabedatei.")
+                        .build());
+        OPTIONS.addOption(
+                Option.builder("xsl").required().hasArg().argName("Datei").type(File.class).desc("XSL Vorlagedatei.")
+                        .build());
+        OPTIONS.addOption(Option.builder("pdf").optionalArg(true).hasArg().argName("Dateiname").type(String.class)
+                .desc("Ausgabeformat PDF.").build());
+        OPTIONS.addOption(Option.builder("html").optionalArg(true).hasArg().argName("Dateiname").type(String.class)
+                .desc("Ausgabeformat HTML.").build());
+        OPTIONS.addOption(
+                Option.builder("h").longOpt("help").optionalArg(true).hasArg(false).desc("Diese Nachricht.").build());
+
+        HELP_FORMATTER.setSyntaxPrefix("Programmaufruf: ");
     }
 
-    @SuppressWarnings("unused")
-    private void convertToHTML(final String xml, final String xsl)
+    public static void main(String[] args)
+            throws IOException, SAXException, TransformerException, ParserConfigurationException
+    {
+        File xml = null;
+        File xsl = null;
+        File fopConfig = new File(FOP_CONFIG_FILE_NAME);
+
+        try
+        {
+            CommandLine cmd = COMMAND_LINE_PARSER.parse(OPTIONS, args);
+            if (args.length < 5 || cmd.hasOption("h") ||
+                    (cmd.hasOption("pdf") && cmd.hasOption("html")) || (!cmd.hasOption("pdf") && !cmd.hasOption("html")))
+            {
+                printUsageAndExit();
+            }
+            if (cmd.hasOption("xml"))
+            {
+                xml = (File)cmd.getParsedOptionValue("xml");
+            }
+            if (cmd.hasOption("xsl"))
+            {
+                xsl = (File)cmd.getParsedOptionValue("xsl");
+            }
+            if (xml == null || !xml.exists() || xsl == null || !xsl.exists())
+            {
+                printUsageAndExit();
+            }
+            if (cmd.hasOption("pdf") && !cmd.hasOption("html"))
+            {
+                if (cmd.hasOption("c"))
+                {
+                    fopConfig = (File)cmd.getParsedOptionValue("c");
+                }
+                new XmlXslFoRenderer().convertToPDF(xml, xsl, fopConfig, cmd.getOptionValue("pdf", null));
+            }
+            if (cmd.hasOption("html") && !cmd.hasOption("pdf"))
+            {
+                if (xml != null && xml.exists() && xsl != null && xsl.exists())
+                {
+                    new XmlXslFoRenderer().convertToHTML(xml, xsl, cmd.getOptionValue("html", null));
+                }
+            }
+        }
+        catch (ParseException e)
+        {
+            printUsageAndExit();
+        }
+    }
+
+    private void convertToHTML(final File datafile, final File stylesheet, String html)
             throws IOException, SAXException, ParserConfigurationException, TransformerException
     {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
         //factory.setNamespaceAware(true);
         //factory.setValidating(true);
-        File stylesheet = new File(xsl);
-        File datafile = new File(xml);
 
         DocumentBuilder builder = factory.newDocumentBuilder();
         Document document = builder.parse(datafile);
@@ -67,25 +135,43 @@ public class XmlXslFoRenderer
         Transformer transformer = tFactory.newTransformer(stylesource);
 
         DOMSource source = new DOMSource(document);
-        StreamResult result = new StreamResult(System.out);
-        transformer.transform(source, result);
+        if (html == null)
+        {
+            html = (datafile.getParent() != null ? datafile.getParent() : ".") + "/" + datafile.getName().replace(".xml", ".html");
+        }
+        if (!html.endsWith(".html"))
+        {
+            html = html.concat(".html");
+        }
+        File file = new File(html);
+        try (final FileWriter fw = new FileWriter(file)) {
+            StreamResult result = new StreamResult(fw);
+            transformer.transform(source, result);
+        }
+        // open created html file in browser
+        if (Desktop.isDesktopSupported())
+        {
+            Desktop.getDesktop().browse(file.toURI());
+        }
     }
 
-    private void convertToPDF(final String xml, final String xsl, String pdf)
+    private void convertToPDF(final File xmlFile, final File xsltFile, final File fopConfig, String pdf)
             throws IOException, SAXException, TransformerException
     {
-        File xsltFile = new File(xsl);
-        File xmlFile = new File(xml);
         StreamSource xmlSource = new StreamSource(xmlFile);
         FopFactory fopFactory = FopFactory.newInstance();
-        if (Files.exists(Paths.get("fop-config.xconf")))
+        if (fopConfig != null && fopConfig.exists())
         {
-            fopFactory.setUserConfig(new File("fop-config.xconf"));
+            fopFactory.setUserConfig(fopConfig);
         }
         FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
         if (pdf == null)
         {
             pdf = (xmlFile.getParent() != null ? xmlFile.getParent() : ".") + "/" + xmlFile.getName().replace(".xml", ".pdf");
+        }
+        if (!pdf.endsWith(".pdf"))
+        {
+            pdf = pdf.concat(".pdf");
         }
         FileOutputStream out = new FileOutputStream(pdf);
         Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, out);
@@ -94,5 +180,11 @@ public class XmlXslFoRenderer
         SAXResult result = new SAXResult(fop.getDefaultHandler());
         transformer.transform(xmlSource, result);
         out.close();
+    }
+
+    private static void printUsageAndExit()
+    {
+        HELP_FORMATTER.printHelp(80, USAGE, HEADER, OPTIONS, FOOTER, true);
+        System.exit(0);
     }
 }
